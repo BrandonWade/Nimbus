8404e6a2c6e2842f6f3eb0137ec0beeb737f1fde
Brandon Wade <brandon_wade2005@hotmail.com>
2015-10-07 23:29:12 -0500

    Adds initial weather lookup data.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,6 @@
  */
 function getUserCoordinates() {
     var container = $("#container");
-    var result;
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(processCoordinates);
@@ -23,7 +22,6 @@ function processCoordinates(position) {
         cache: false,
         crossDomain: true,
         success: function(data) {
-            container.append(data);
             console.log(data);
             processWeatherData(data);
         },
@@ -41,7 +39,10 @@ function processWeatherData(weatherDataObject) {
     var date = new Date(weatherDataArray[0].dt * 1000);
     var newDate = null;
     var weekday = "";
-    var temperature = 0;
+    var conditionText = "";
+    var conditionIcon = "";
+    var maxTemperature = 0;
+    var minTemperature = 0;
 
     for (var i = 0; i < weatherDataArray.length; i++) {
         newDate = new Date(weatherDataArray[i].dt * 1000);
@@ -49,25 +50,19 @@ function processWeatherData(weatherDataObject) {
         if (compareDates(date, newDate)) {
             temperatureArray.push(Math.round(weatherDataArray[i].main.temp_max));
         } else {
-            temperatureArray.sort();
-
+            var skyConditions = getSkyConditions(weatherDataArray[i]);
             weekday = WEEKDAY_NAMES[date.getDay()];
-            temperature = Math.max(...temperatureArray);
-            //console.log(weekday + " " + temperature);
-            weeklyData.push([weekday, temperature]);
+            conditionText = skyConditions.description;
+            conditionIcon = skyConditions.icon;
+            maxTemperature = Math.max(...temperatureArray);
+            minTemperature = Math.min(...temperatureArray);
+
+            var dailyData = {weekday: weekday, conditionText: conditionText, conditionIcon: conditionIcon, maxTemperature: maxTemperature, minTemperature: minTemperature};
+            weeklyData.push(dailyData);
 
             temperatureArray = [];
             date = newDate;
         }
-    }
-
-    if (temperatureArray != []) {
-        temperatureArray.sort();
-
-        weekday = WEEKDAY_NAMES[date.getDay()];
-        temperature = Math.max(...temperatureArray);
-        weeklyData.push([weekday, temperature]);
-        //console.log(weekday + " " + temperature);
     }
 
     console.log(weeklyData);
@@ -84,12 +79,50 @@ function roundToNearestMultiple(value, multiple) {
     return multiple * (Math.round(value / multiple));
 }
 
+function getSkyConditions(weatherData) {
+    var weatherCode = String(weatherData.weather[0].id);
+    var weatherCodePrefix = "";
+    var skyConditions = null;
+
+    if (weatherCode.length > 0) {
+        weatherCodePrefix = weatherCode.substr(0, 1);
+    }
+
+    switch (weatherCodePrefix) {
+        case "2" : // Code is in 2XX range
+            skyConditions = {description: "Thunderstorm", icon: "11d.png"};
+            break;
+        case "3" : // Code is in 3XX range
+            skyConditions = {description: "Drizzle", icon: "09d.png"};
+            break;
+        case "5" : // Code is in 5XX range
+            skyConditions = {description: "Rain", icon: "10d.png"};
+            break;
+        case "6" : // Code is in 6XX range
+            skyConditions = {description: "Snow", icon: "13d.png"};
+            break;
+        case "7" : // Code is in 7XX range
+            // Atmosphere
+            break;
+        case "8" : // Code is in 8XX range
+            skyConditions = {description: "Clouds", icon: "03d.png"};
+            break;
+        case "9" : // Code is in 9XX range
+            // Other / Extreme
+            break;
+        default :
+            skyConditions = null;
+            break;
+    }
+
+    return skyConditions;
+}
+
 function outputForecast(weeklyData) {
     var forecastList = $("#forecastList");
 
     for (var i = 0; i < weeklyData.length; i++) {
-
-        forecastList.append("<li><div>" + weeklyData[i][0] + "</div><div>" + weeklyData[i][1] + "</div></li>");
+        forecastList.append("<li><div>" + weeklyData[i].weekday + "</div><div>" + weeklyData[i].conditionText + "</div><div>" + weeklyData[i].maxTemperature + "</div><div>" + weeklyData[i].minTemperature + "</div>" + "</li>");
     }
 
     $("#forecastListContainer").append(forecastList);
